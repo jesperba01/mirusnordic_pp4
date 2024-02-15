81e006d9102531d750ce1ed7c1fc247de384e6c2
jesper Barthelsson <JEsperbarthelsson@gmail.com>
2024-02-15 10:52:53 +0000

    Fix typo on whitenoise middleware

--- main/settings.py
+++ main/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middlewear.WhiteNoiseMiddlewear',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
