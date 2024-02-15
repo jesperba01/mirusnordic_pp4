f0e95db719359a376fb7cf85cc931f71991aed43
jesper Barthelsson <JEsperbarthelsson@gmail.com>
2024-02-15 10:58:03 +0000

    Disable urls for allauth and summernote

--- main/urls.py
+++ main/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("", include("home.urls"), name="home"),
     path('admin/', admin.site.urls),
+    # path('summernote/', include('django_summernote.urls')),
+    # path("accounts/", include("allauth.urls")),
+    path("treatments/", include("treatments.urls"), name="treatment-urls"),
 ]
