f43d02115ae768f82fdce84e55f0a3d9fff8c3d9
jesper Barthelsson <JEsperbarthelsson@gmail.com>
2024-02-16 16:16:51 +0000

    Add style to index.html

--- main/urls.py
+++ main/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("home.urls"), name="home"),
     path('admin/', admin.site.urls),
     # path('summernote/', include('django_summernote.urls')),
     # path("accounts/", include("allauth.urls")),
+    path("", include("home.urls"), name="home-urls"),
     path("treatments/", include("treatments.urls"), name="treatment-urls"),
 ]
