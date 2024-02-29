9044ccfdc9ce25cfedbaf7d35a81d0c283e4dbeb
jesper Barthelsson <JEsperbarthelsson@gmail.com>
2024-02-29 14:30:41 +0000

    Add a feature to change password and email from dashboard.html

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.get_dashboard, name="dashboard"),
     path("cancel_booking/<int:booking_id>/", views.cancel_booking, name="cancel_booking"),
+    path("change_email/", views.change_email, name="change_email"),
+    path("change_password/", views.change_password, name="change_password"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, redirect
-from treatments.models import Booking
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
+from treatments.models import Booking
+from .forms import EmailChangeForm
+
+@login_required
 def get_dashboard(request):
     if not request.user.is_authenticated:
         return redirect('login')  # Redirect to login page if user is not authenticated
@@ -10,7 +18,7 @@ def get_dashboard(request):
     context = {'bookings': bookings}
     return render(request, template, context)
 
-
+@login_required
 def cancel_booking(request, booking_id):
     # Retrieve the booking object
     booking = Booking.objects.get(pk=booking_id)
@@ -22,3 +30,30 @@ def cancel_booking(request, booking_id):
 
     # Redirect to the dashboard or any other page after cancellation
     return redirect('dashboard')
+
+@login_required
+def change_email(request):
+    if request.method == 'POST':
+        form = EmailChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your email has been updated successfully.')
+            return redirect('dashboard')
+    else:
+        form = EmailChangeForm(instance=request.user)
+    return render(request, 'dashboard/change-email.html', {'form': form})
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Update the user's session to prevent them from being logged out
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password has been successfully updated.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'dashboard/change_password.html', {'form': form})
